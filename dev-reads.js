1d9f34213ab324b3f5e6c7a44b67451dcf9fc622
Stewart Duffy <duffy.stewart@gmail.com>
2018-08-22 09:19:10 +1200

    Create dev-reads.js

---
+++ dev-reads.js
@@ -0,0 +1,25 @@
+# Dev reads
+
+A list of links so good, they deserve more than a bookmark!
+
+### Resources
+- [scalable-css-reading-list](https://github.com/davidtheclark/scalable-css-reading-list/blob/master/README.md) The list that inspired this!
+
+### Linting
+- [Sass linting](http://davidtheclark.com/scss-lint-styleguide/)
+
+### Developer Environment
+- [Mac set up script](https://github.com/MoOx/setup/tree/master/setupsh/)
+
+### Git
+- [git - the simple guide](http://rogerdudler.github.io/git-guide/)
+
+### You might not need x... 
+- [jQuery](http://youmightnotneedjquery.com/)
+- [Underscore, lodash](https://www.reindex.io/blog/you-might-not-need-underscore/)
+
+### Esoteric ES
+- [Data structures for JS](https://yomguithereal.github.io/mnemonist/)
+
+Pandoc & LaTeX
+- [Pandoc](/pandoc.md)
